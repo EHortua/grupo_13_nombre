26714b7aa0460fd4e7b0ad56e7db96b744b8991a
JulianaLezcano <juliana.v.lezcano@hotmail.com>
2021-07-07 21:51:19 -0300

    actualizando

--- app.js
+++ app.js
@@ -12,3 +12,6 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'views/index.html'))
 });
 
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'views/productCart.html'))
+})
